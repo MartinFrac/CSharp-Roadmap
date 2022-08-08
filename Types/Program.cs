d39d0fd6d8f3e42da83e07bc5dd46bcf380815d9
Marcin Frackiewicz <marcin.jarek.frackiewicz@gmail.com>
2022-08-08 09:19:49 +0100

    Extracted logic to types class

--- Types/Program.cs
+++ Types/Program.cs
@@ -31,31 +31,5 @@ sb.AppendLine(String.Format("{0,10} {1,10}","boolT:", boolT));
 sb.AppendLine(String.Format("{0,10} {1,10}","decimalT:", decimalT));
 Console.WriteLine(sb);
 
-var infinity = 1.0 / 0.0;
-var negativeInfinity = -1.0 / 0.0;
-Console.WriteLine($"Infinity: {infinity}");
-Console.WriteLine($"negative Infinity {negativeInfinity}");
-
-var intMax = int.MaxValue;
-var intMin = int.MinValue;
-Console.WriteLine($"max value {intMax}");
-Console.WriteLine($"min value {intMin}");
-Console.WriteLine($"max value +1 {intMax+1}");
-Console.WriteLine($"min value -1 {intMin-1}");
-
-var decimalMax = decimal.MaxValue;
-Console.WriteLine($"decimal max: {decimalMax}");
-// Console.WriteLine($"decimal max + 1: {decimalMax+1}");
-
-int? intNullable = 5;
-System.Nullable<int> intNull = null;
-var hasValue = intNullable.HasValue;
-var hasValue2 = intNull.HasValue;
-
-Console.WriteLine($"has value: {hasValue}");
-Console.WriteLine($"has value: {hasValue2}");
-
-int intBox = 32;
-object box = intBox;
-Console.WriteLine($"box: {intBox}");
-if (box is int) Console.WriteLine("its an int");
+var typesRepo = new Types();
+typesRepo.ShowRun();
--- Types/Types.cs
+++ Types/Types.cs
@@ -1,15 +1,41 @@
 public class Types
 {
+  public void ShowRun()
+  {
+    var intMax = int.MaxValue;
+    var intMin = int.MinValue;
+    Console.WriteLine($"max value {intMax}");
+    Console.WriteLine($"min value {intMin}");
+    Console.WriteLine($"max value +1 {intMax + 1}");
+    Console.WriteLine($"min value -1 {intMin - 1}");
+
+    var decimalMax = decimal.MaxValue;
+    Console.WriteLine($"decimal max: {decimalMax}");
+    // Console.WriteLine($"decimal max + 1: {decimalMax+1}");
+
+    int? intNullable = 5;
+    System.Nullable<int> intNull = null;
+    var hasValue = intNullable.HasValue;
+    var hasValue2 = intNull.HasValue;
+    Console.WriteLine($"has value: {hasValue}");
+    Console.WriteLine($"has value: {hasValue2}");
+
+    int intBox = 32;
+    object box = intBox;
+    Console.WriteLine($"box: {intBox}");
+    if (box is int) Console.WriteLine("its an int");
+  }
+
   public double? GetInfinity()
   {
     return null;
   }
-  
+
   public double? GetNegativeInfinity()
   {
     return null;
   }
-  
+
   public int? GetMaxInt()
   {
     return null;
